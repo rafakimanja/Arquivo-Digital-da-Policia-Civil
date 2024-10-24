1b6a24a98a6232afb555252c913271ad7f657cbf
Rafael Lopes Ens <rafaellopes.ifrs@gmail.com>
2024-10-24 02:06:23 -0300

    create login form

---
+++ app-react/src/components/FormLogin.jsx
@@ -0,0 +1,36 @@
+import logoPolicia from '../assets/Logo-PC.png'
+import olhoVisivel from '../assets/olho-aberto2.svg'
+import olhoInvisivel from '../assets/olho-fechado.svg'
+import './FormLogin.css'
+
+
+const FormLogin = () => {
+    return (
+        <div className='background'>
+            <div className='div-image'>
+                <img src={logoPolicia} alt="" />
+            </div>
+            <div className='div-form'>
+                <h2>Login</h2>
+                <form action="#" method="post">
+                    <div className='input-group'>
+                        <input type="text" name="" id=""  required/>
+                        <label htmlFor="">RG</label>
+                    </div>
+                    <div className='input-group'>
+                        <input type="password" name="" id="input-senha"  required />
+                        <label htmlFor="">SENHA</label>
+                        <img src={olhoVisivel} alt="" className='eye-icon ' />
+                    </div>
+                    <a href="#">Esqueceu a Senha?</a>
+                    <div className='button-group'>
+                        <button type="submit">ENTRAR</button>
+                        <button>CADASTRAR</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default FormLogin
